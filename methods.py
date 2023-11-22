ad51a3fede627dee7dff96b452bf1e81cacee651
anna.borowski <anna.borowski@tu-dortmund.de>
2023-11-22 19:13:39 +0100

    More models in methods.py

--- methods.py
+++ methods.py
@@ -2,8 +2,30 @@ from pykeen import models
 
 
 def model_init(model, dataset, seed=None):
-    if model == 'TransE':
-        model = models.TransE(triples_factory=dataset.training, random_seed=seed)
-    elif model == 'RotatE':
-        model = models.RotatE(triples_factory=dataset.training, random_seed=seed)
-    return model
+    model_classes = {
+        'ComplEx': models.ComplEx,
+        'ConvE': models.ConvE,
+        'ConvKB': models.ConvKB,
+        'DistMult': models.DistMult,
+        'ERMLP': models.ERMLP,
+        'HolE': models.HolE,
+        'KG2E': models.KG2E,
+        'MuRE': models.MuRE,
+        'NTN': models.NTN,
+        'ProjE': models.ProjE,
+        'QuatE': models.QuatE,
+        'RESCAL': models.RESCAL,
+        'RotatE': models.RotatE,
+        'SE': models.SE,
+        'SimplE': models.SimplE,
+        'TransD': models.TransD,
+        'TransE': models.TransE,
+        'TransH': models.TransH,
+        'TransR': models.TransR,
+        'TuckER': models.TuckER,
+        'UM': models.UM,
+    }
+    if model in model_classes:
+        return model_classes[model](triples_factory=dataset.training, random_seed=seed)
+    else:
+        raise ValueError(f"Unsupported model: {model}")
