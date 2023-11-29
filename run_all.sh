aaa82ec1143dce75a87e63c2b0c6e4c1bbe23b16
borowski <borowski.a@t-online.de>
2023-11-29 11:28:11 +0000

    Add models in bash script

--- run_all.sh
+++ run_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for d in "countries"
+for d in "kinships"
 do
-    for m in "TransE"
+    for m in "ComplEx" "ConvE" "ConvKB" "DistMult" "ERMLP" "HolE" "KG2E" "MuRE" "NTN" "ProjE" "QuatE" "RESCAL" "RotatE" "SE" "SimplE" "TransD" "TransE" "TransH" "TransR" "TuckER" "UM"
     do
         for s in "42" "135" "468"
         do
-        python run.py --dataset $d --model $m --seed $s --output-dir $1
+        python run2.py --dataset $d --model $m --seed $s --output-dir $1
         done
     done
 done
